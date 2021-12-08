e5bb95a36148caa43e5732338bcaf677be76abf7
Miguel Ángel Latre Abadía <latre@unizar.es>
2021-12-08 19:21:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Trabajo obligatorio de la asignatura Programación 1
+
+## Curso 2021-22
+
+### Escuela de Ingeniería y Arquitectura. Universidad de Zaragoza
+
+Datos y código fuente de partida para el trabajo obligatorio de Programación 1.
+
+Puede clonarse o descargarse en formato ZIP y abrirse en Visual Studio Code, abriendo la carpeta ``trabajo-2021-22`` a través de la opción de menú ``File > Open Folder...``.
+
+Todos los ficheros del directorio ``datos`` están codificados utilizando UTF-8.
---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+/******************************************************************************\
+ * Programación 1. Trabajo obligatorio curso 2021-22
+ * Autores: ¡¡¡PONED AQUÍ VUESTROS NOMBRES!!!
+ * Ultima revisión: ¡¡¡!!!
+ * Resumen: ¡¡¡!!!
+ * Nota: El código de este programa está repartido en varios módulos.
+ *       Para compilarlo, hay que ejecutar el comando
+ *           make all
+ *       o, en Windows,
+ *           mingw32-make all
+ *       o ejecutar la tarea "Compilar «electricidad»" de VSC.
+ * 
+ *       Para ejecutarlo, una vez compilado, hay que ejecutar el comando
+ *           bin/electricidad
+ *       o, en Windows,
+ *           bin\electricidad.exe
+ *       o ejecutar la tarea "Ejecutar «electricidad»" de VSC.
+\******************************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "gasto-diario.hpp"
+#include "fichs-electricos.hpp"
+#include "vector-gastos.hpp"
+#include "tarifas-comerciales.hpp"
+using namespace std;
+
+
+/*
+ * Pre:  «f» es «cout» o un flujo de la clase «ofstream» asociado con un fichero
+ *       externo abierto para escritura. Las «numRegs» primeras componentes
+ *       del vector «regDiarios» contienen datos diarios de precios y consumo
+ *       eléctricos de un determinado cliente de nombre «nombreCliente» entre
+ *       los meses «mesInicial» y «mesFinal».
+ * Post: Ha escrito en el flujo «f» el informe sobre consumo eléctrico indicado
+ *       en el enunciado de este trabajo.
+ */
+void escribirInforme(ostream& f,
+                     const GastoDiario regDiarios[], const unsigned numRegs,
+                     const string nombreCliente, 
+                     const unsigned mesInicial, const unsigned mesFinal) {
+}
+
+
+/*
+ * ¡ESCRIBID LA ESPECIFICACIÓN DE ESTA FUNCIÓN!
+ */
+int main() {
+    return 0;
+}
